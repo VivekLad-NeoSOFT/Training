94482b7098d85023c7db200d3f518a8b9ca5c951
VivekLad-NeoSOFT <vivek.lad@neosoftmail.com>
2025-07-02 15:40:38 +0530

    daily data pipe line workinf fine now

--- E-Commerce/dags/ecommerce_pipeline_dag.py
+++ E-Commerce/dags/ecommerce_pipeline_dag.py
@@ -13,9 +13,9 @@ MYSQL_JDBC_URL = 'jdbc:mysql://mysql_source:3306/ecommerce_daily'
 MYSQL_USER = 'admin'
 MYSQL_PASSWORD = 'p@ssw0rd'
 
-POSTGRES_JDBC_URL = 'jdbc:postgresql://postgres_dw:5432/ecommerce_warehouse'
+POSTGRES_JDBC_URL = 'jdbc:postgresql://postgres:5432/ecommerce_warehouse'
 POSTGRES_USER = 'admin'
-POSTGRES_PASSWORD = 'p@ssw0rd'
+POSTGRES_PASSWORD = 'password'
 
 # Tables
 MYSQL_TABLES = ['orders', 'customers', 'products']
@@ -25,8 +25,8 @@ MYSQL_TABLES = ['orders', 'customers', 'products']
     dag_id='ecommerce_data_pipeline',
     default_args={'owner': 'airflow', 'depends_on_past': False},
     description='Daily E-commerce Data Pipeline',
-    # schedule='@daily',
-    schedule=None,
+    schedule='@daily',
+    # schedule=None,
     start_date=datetime(2024, 1, 1),
     tags=['ecommerce', 'medallion']
 )
@@ -36,7 +36,7 @@ def ecommerce_data_pipeline():
     bronze_tasks = []
     for table in MYSQL_TABLES:
         bronze_task = SparkSubmitOperator(
-            task_id=f'ingest_{table}_to_bronze',
+            task_id=f'bronze_ingestion_{table}',
             application=f'{PYSPARK_APP_BASE_PATH}/bronze_ingestion.py',
             conn_id='dp_conn',
             application_args=[
@@ -51,7 +51,47 @@ def ecommerce_data_pipeline():
         )
         bronze_tasks.append(bronze_task)
 
-    bronze_tasks
+    silver_task = SparkSubmitOperator(
+        task_id='silver_processing',
+        application=f'{PYSPARK_APP_BASE_PATH}/silver_processing.py',
+        conn_id='dp_conn',
+        application_args=[
+            BRONZE_BASE_PATH,
+            SILVER_BASE_PATH,
+            '{{ ds }}'
+        ],
+        verbose=True,
+    )
+
+    gold_task = SparkSubmitOperator(
+        task_id='gold_aggregation',
+        application=f'{PYSPARK_APP_BASE_PATH}/gold_aggregation.py',
+        conn_id='dp_conn',
+        application_args=[
+            SILVER_BASE_PATH,
+            GOLD_BASE_PATH,
+            '{{ ds }}'
+        ],
+        verbose=True,
+    )
+
+    load_gold_to_dwh = SparkSubmitOperator(
+        task_id='sales_summary_to_postgres_dw',
+        application=f'{PYSPARK_APP_BASE_PATH}/load_to_warehouse.py',
+        conn_id='dp_conn',
+        application_args=[
+            f'{GOLD_BASE_PATH}/sales_daily_summary/' + '{{ds}}',
+            POSTGRES_JDBC_URL,
+            POSTGRES_USER,
+            POSTGRES_PASSWORD,
+            'facts.fact_sales_summary',
+            '{{ ds }}'
+        ],
+        jars='/opt/bitnami/spark/jars/postgresql-42.7.7.jar',
+        verbose=True,
+    )
+
+    bronze_tasks >> silver_task >> gold_task >> load_gold_to_dwh
 
 
 ecommerce_data_pipeline()
